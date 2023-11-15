1085f74ff14d172783150237e64400e4cf468d01
Никита Сухарев <sukharev1998@yandex.ru>
2023-11-15 16:23:19 +0300

    Удачи мне

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -62,12 +62,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
-      'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
-      'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-      'HOST': os.getenv('DB_HOST', 'db'),
-      'PORT': os.getenv('DB_PORT', 5432)
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
