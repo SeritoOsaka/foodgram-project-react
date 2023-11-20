806e3f6404935330ccac4bb24bf329328951583e
Никита Сухарев <sukharev1998@yandex.ru>
2023-11-20 12:28:46 +0300

    Yda4i

--- backend/recipes/management/commands/import_csv.py
+++ backend/recipes/management/commands/import_csv.py
@@ -8,21 +8,18 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         file_path = 'data/ingredients.csv'
-
         try:
             with open(file_path, encoding='UTF-8') as file:
-                reader = csv.DictReader(file)
-                ingredients_to_create = [
-                    Ingredient(name=row['name'],
-                               measurement_unit=row['measurement_unit'])
-                    for row in reader
-                ]
+                ingredients_list = []
+                for row in csv.reader(file):
+                    name, measurement_unit = row
+                    ingredient = Ingredient(name=name,
+                                            measurement_unit=measurement_unit)
+                    ingredients_list.append(ingredient)
 
-            Ingredient.objects.bulk_create(ingredients_to_create)
-            print('Импорт данных завершен!')
-
+                Ingredient.objects.bulk_create(ingredients_list)
+                print('Импорт данных завершен!')
         except FileNotFoundError:
             print(f'File not found: {file_path}')
-
         except Exception as error:
             print(f'An error occurred: {error}')
