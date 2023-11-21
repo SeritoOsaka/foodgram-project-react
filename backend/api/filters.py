6e90cd63638c3afd61fd3934ac8ac857cf57f342
Никита Сухарев <sukharev1998@yandex.ru>
2023-11-21 15:39:20 +0300

    da

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -29,10 +29,10 @@ class RecipeFilter(filters.FilterSet):
 
     def get_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorites__recipe=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(shopping_carts__user=self.request.user)
+            return queryset.filter(shopping_carts__recipe=self.request.user)
         return queryset
